4104451393ef5fed9efcc8012e68b59b419d823f
au9u5t <1241407552@qq.com>
2025-03-27 17:13:23 +0800

    chore(.gitignore): 更新 .gitignore 文件内容

--- client/cmd/main.go
+++ client/cmd/main.go
@@ -1,5 +1,26 @@
 package cmd
 
 func main() {
-	//conf := config2.Config{}
+	//conf := config.Config{
+	//	Websocket: config.WebsocketConf{
+	//		RemoteAddr: "",
+	//		RemotePort: 0,
+	//	},
+	//	Db: config2.DbConf{
+	//		Host:       "139.159.140.145",
+	//		Port:       5432,
+	//		Username:   "postgres",
+	//		Password:   "123qweASD!@##@!!@#",
+	//		Dbname:     "dewu",
+	//		TimeOut:    100000000,
+	//		DbFilePath: "",
+	//		DbType:     "postgres",
+	//	},
+	//}
+
+	//server, err := server.NewServer(&conf)
+	//if err != nil {
+	//	panic(err)
+	//}
+
 }

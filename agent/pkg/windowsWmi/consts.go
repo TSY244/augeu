cfc2225fcdfc1fc9ce1fc1175a8c8c9077c5b33b
au9u5t <1241407552@qq.com>
2025-04-09 17:22:41 +0800

    feat: 完成了服务信息，定时任务，用户信息的获取

--- agent/pkg/windowsWmi/consts.go
+++ agent/pkg/windowsWmi/consts.go
@@ -5,4 +5,6 @@ const (
 	QueryOsNameKey    = "SELECT Caption FROM Win32_OperatingSystem"
 	QueryOsVersionKey = "SELECT Version FROM Win32_OperatingSystem"
 	QueryHotFixKey    = "SELECT Description, HotFixID, InstalledBy, InstalledOn FROM Win32_QuickFixEngineering"
+	QueryUsersKey     = "SELECT Name, Description, LocalAccount, SID FROM Win32_UserAccount"
+	QueryServiceKey   = "SELECT Name, DisplayName, State, StartMode, StartName FROM Win32_Service"
 )
--- agent/pkg/windowsWmi/core.go
+++ agent/pkg/windowsWmi/core.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"fmt"
 	"github.com/StackExchange/wmi"
+	"log"
 )
 
 // QueryUuid 查询系统UUID
@@ -60,6 +61,60 @@ func baseSlice[T any](resultSet *[]T, queryKey string) error {
 func QueryHotFix() ([]Win32_QuickFixEngineering, error) {
 	var dst []Win32_QuickFixEngineering
 	if err := baseSlice(&dst, QueryHotFixKey); err != nil {
+		return nil, err
+	}
+	return dst, nil
+}
+
+func QueryUsers() ([]Win32_UserAccount, error) {
+	var dst []Win32_UserAccount
+	if err := baseSlice(&dst, QueryUsersKey); err != nil {
+		return nil, err
+	}
+	return dst, nil
+}
+
+func QueryScheduledTasks() ([]Win32_ScheduledTask, error) {
+	namespace := "Root\\Microsoft\\Windows\\TaskScheduler"
+
+	// 准备接收查询结果
+	var tasks []win32_ScheduledTask
+
+	// 执行 WMI 查询
+	query := wmi.CreateQuery(&tasks, "", "MSFT_ScheduledTask")
+	err := wmi.QueryNamespace(query, &tasks, namespace)
+	if err != nil {
+		log.Fatalf("WMI 查询失败: %v (可能需要管理员权限)", err)
+	}
+
+	stateMap := map[int]string{
+		1: "禁用",
+		3: "就绪",
+		4: "运行中",
+	}
+
+	var result []Win32_ScheduledTask
+
+	for _, task := range tasks {
+		state, ok := stateMap[task.State]
+		if !ok {
+			state = "未知"
+		}
+		result = append(result, Win32_ScheduledTask{
+			TaskName:    task.TaskName,
+			Author:      task.Author,
+			State:       state,
+			Description: task.Description,
+			TaskPath:    task.TaskPath,
+			URI:         task.URI,
+		})
+	}
+	return result, nil
+}
+
+func QueryServices() ([]Win32_Service, error) {
+	var dst []Win32_Service
+	if err := baseSlice(&dst, QueryServiceKey); err != nil {
 		return nil, err
 	}
 	return dst, nil

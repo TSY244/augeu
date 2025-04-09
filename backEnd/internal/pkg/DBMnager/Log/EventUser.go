949695d1e1d31c145461c9932137d10e9180d381
au9u5t <1241407552@qq.com>
2025-04-09 18:38:50 +0800

    refactor: 重构了EventUser 表结构

--- backEnd/internal/pkg/DBMnager/Log/EventUser.go
+++ backEnd/internal/pkg/DBMnager/Log/EventUser.go
@@ -6,17 +6,18 @@ import (
 )
 
 type EventUser struct {
-	CreateAt     time.Time      `gorm:"autoCreateTime"`
-	DeleteAt     gorm.DeletedAt `gorm:"index"`
-	ID           uint           `gorm:"primaryKey;autoIncrement"`
-	UUID         string         `gorm:"type:varchar(255);column:uuid"`
-	EventID      string         `gorm:"column:event_id"`
-	SourceName   string         `gorm:"column:Source_name"`
-	SourceDomain string         `gorm:"column:Source_domain"`
-	TargetName   string         `gorm:"column:Target_name"`
-	TargetDomain string         `gorm:"column:Target_domain"`
-	MemberSid    string         `gorm:"column:MemberSid"`
-	Description  string         `gorm:"type:text"`
+	CreateAt      time.Time      `gorm:"autoCreateTime"`
+	DeleteAt      gorm.DeletedAt `gorm:"index"`
+	ID            uint           `gorm:"primaryKey;autoIncrement"`
+	UUID          string         `gorm:"type:varchar(255);column:uuid"`
+	EventID       string         `gorm:"column:event_id"`
+	SourceName    string         `gorm:"column:Source_name"`
+	SourceDomain  string         `gorm:"column:Source_domain"`
+	SourceUserSid string         `gorm:"column:Source_user_sid"`
+	TargetName    string         `gorm:"column:Target_name"`
+	TargetDomain  string         `gorm:"column:Target_domain"`
+	TargetUserSid string         `gorm:"column:Target_user_sid"`
+	Description   string         `gorm:"type:text"`
 }
 
 func (EventUser) TableName() string {
